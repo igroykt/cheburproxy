382a8f1355614234d39bab05a4562873fc3fba10
igroykt <artofmotion.igroman@gmail.com>
2026-03-20 11:18:19 +0500

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,6 +4,7 @@ LAN_ADDR="192.168.1.10"
 LAN_IFACE="eno1"
 
 ip rule del fwmark 0x1 >/dev/null 2>&1
+ip rule del fwmark 0x2 >/dev/null 2>&1
 ip rule del from $LAN_ADDR table default >/dev/null 2>&1
 ip rule del to $LAN_ADDR table default >/dev/null 2>&1
 
